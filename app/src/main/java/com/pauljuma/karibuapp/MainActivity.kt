87b9bc2c4e7734aa1151f78ee50b2d8911e6a5bc
James Ambole <91792139+Wakambo@users.noreply.github.com>
2022-11-21 10:37:42 +0300

    login

--- app/src/main/java/com/pauljuma/karibuapp/MainActivity.kt
+++ app/src/main/java/com/pauljuma/karibuapp/MainActivity.kt
@@ -2,12 +2,15 @@ package com.pauljuma.karibuapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pauljuma.karibuapp.fragments.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportActionBar?.hide()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container,LoginFragment())
+            .commit()
     }
 }
